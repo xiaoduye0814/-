17a8b0722fa26bc6a9852562d8905950f7f78b51
付佳 <fujia@thunisoft.com>
2019-07-25 17:46:36 +0800

    增加学习文件

---
+++ 赋权.sql
@@ -0,0 +1,27 @@
+create role api encrypted password 'tusc99' nosuperuser nocreatedb nocreaterole inherit login;
+alter role api set search_path to maindata, pythondata;
+grant all on all tables in schema[模式] db_jwfwzzzd to [用户] zzfwpt;
+grant usage on schema maindata to api;
+grant usage on schema pythondata to api;
+grant select  on table maindata.t_area  to api;
+grant select  on table maindata.t_maindata_court_execution_situation  to api;
+grant select  on table maindata.t_maindata_execute  to api;
+grant select  on table maindata.t_maindata_judgement to api;
+grant select  on table maindata.t_maindata_judgement_ext to api;
+grant select  on table maindata.t_maindata_mandatory_action to api;
+grant select  on table maindata.t_maindata_product_property to api;
+grant select  on table maindata.t_maindata_shaoxing_property to api;
+
+grant select  on table pythondata.t_base_f_test to api;
+grant select  on table pythondata.t_basic to api;
+grant select  on table pythondata.t_basic_25_new to api;
+grant select  on table pythondata.t_basic_copy to api;
+grant select  on table pythondata.t_bjrsx to api;
+grant select  on table pythondata.t_dsrinfo_f to api;
+grant select  on table pythondata.t_extendlimit to api;
+grant select  on table pythondata.t_hfajxx to api;
+grant select  on table pythondata.t_kcd to api;
+grant select  on table pythondata.t_qsxx to api;
+grant select  on table pythondata.yuliu_t_qsxx to api;
+grant select  on table pythondata.yuliu_t_qsxx01 to api;
+grant select  on table pythondata.z_qsxx_002 to api;
